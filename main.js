1b42e645aa795bb905a7dabaf61d21f331bbc8ff
Another <89122911+Dimas01u@users.noreply.github.com>
2022-09-15 14:30:16 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+window.addEventListener("scroll",function(){
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', this.window.scrollY > 0);
+});
+
+const navigation = document.querySelector('nav');
+document.querySelector('.toggle').onclick = function(){
+    this.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
+
 function menuToggle(){
     const toggleMenu = document.querySelector('.toggle');
     const section = document.querySelector('section');
